3e7705589f09f2c3622c600a06f6b3da107b5ddf
Olivie Abigail <olivie.franklova@email.cz>
2015-10-24 20:43:46 +0200

    mereni zaznamu trianglu a dani mereni na web

---
+++ archiv/merenitriangl/vloz_na_web.sh
@@ -0,0 +1,5 @@
+./udelani_grafu.sh
+cp /var/www/obr/test3.svg /var/www/obr/test4.svg
+cp /var/www/obr/test2.svg /var/www/obr/test3.svg
+cp /var/www/obr/test1.svg /var/www/obr/test2.svg
+cp test.svg /var/www/obr/test1.svg
---
+++ mereni0/spust.sh
@@ -0,0 +1,21 @@
+echo vloz desku
+read k
+./poslani_zvuku.sh
+cp test2.dat test3.dat
+
+echo vloz konkavni desku
+read k
+./poslani_zvuku.sh
+cp test2.dat test4.dat
+
+echo vloz konvexni desku
+read k
+./poslani_zvuku.sh
+cp test2.dat test5.dat
+
+echo vloz malou kovovou desku
+read k
+./poslani_zvuku.sh
+cp test2.dat test6.dat
+
+./udelani_grafu.sh
